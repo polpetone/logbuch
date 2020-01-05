5cf9b956eea5209bd50b0ec86aea0254145c3c4b
Stefan Heiden <stefan.heiden@qudosoft.de>
2020-01-05 19:29:02 +0100

    stuck with json serialization of complex objects

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
+from src.domain.Task import Task
 from src.service.TastService import TaskService
 
-if __name__ == "__main__":
-    task_service = TaskService()
 
+def test0():
+    task_service = TaskService()
     task0 = task_service.create_task("Some Task")
     task1 = task_service.create_task("Some Task2")
+    task1.sub_tasks.append(Task("sub task for Task2"))
     task0.change_status(status="CLOSED")
     task_service.save_tasks()
-
     tasks = task_service.get_tasks()
     print(tasks)
+
+
+def test1():
+    task_service = TaskService()
+    tasks = task_service.get_tasks()
+    template = "{0:<30}{1:<40}\n"
+    out = template.format("Date", "Text")
+    for task in tasks:
+        out += template.format(task.date, task.text)
+    return out
+
+
+if __name__ == "__main__":
+    out = test1()
+    print(out)
--- src/domain/Task.py
+++ src/domain/Task.py
@@ -1,7 +1,6 @@
 import uuid
 from datetime import datetime
 import json
-
 from src.domain.TaskStatus import TaskStatus
 
 
@@ -11,11 +10,18 @@ class Task:
                  text,
                  status=TaskStatus(),
                  uid=str(uuid.uuid4()),
-                 date=datetime.now().isoformat()):
+                 date=datetime.now().isoformat(),
+                 sub_tasks=None):
+        if sub_tasks is None:
+            sub_tasks = list()
         self.text = text
         self.status = status
         self.uid = uid
         self.date = date
+        self.sub_tasks = sub_tasks
+
+    def add_sub_task(self, task):
+        self.sub_tasks.append(task)
 
     def change_status(self, status):
         self.status = TaskStatus(status, datetime.now().isoformat())
