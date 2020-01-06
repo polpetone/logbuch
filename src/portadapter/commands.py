bbbd38e27ccbdada16276b9a48ed223ef7fa8f7c
Stefan Heiden <stefan.heiden@qudosoft.de>
2020-01-06 19:54:43 +0100

    add sub task

--- src/portadapter/commands.py
+++ src/portadapter/commands.py
@@ -1,5 +1,6 @@
 import click
 
+from src.domain.Task import Task
 from src.portadapter.TasksView import TasksView
 from src.service.TastService import TaskService
 
@@ -21,9 +22,8 @@ def tasks():
 
 
 @logbuch.command()
-@click.option("--nr", type=click.IntRange(0, 10), prompt="nr", help="Select a task by number")
+@click.option("--nr", type=click.IntRange(0, 20), prompt="nr", help="Select a task by number")
 def task(nr):
-    click.echo("Selected {}".format(nr))
     tasks_view = task_service.get_tasks_view()
     task_view = tasks_view.get_task_view_by_number(nr)
     if task_view:
@@ -36,3 +36,20 @@ def add_task(text):
     task_service.create_task(text)
     task_service.save_tasks()
     print_simple_task_list()
+
+
+@logbuch.command()
+@click.option("--nr", type=click.IntRange(0, 20), prompt="nr", help="Select a task by number")
+@click.option("--text", prompt="Text", help="Text of the new Task")
+def add_sub_task(nr, text):
+    tasks_view = task_service.get_tasks_view()
+    task_view = tasks_view.get_task_view_by_number(nr)
+    if task_view:
+        sub_task = Task(text)
+        task_view.task.add_sub_task(sub_task)
+        task_service.save_tasks()
+        print_simple_task_list()
+    else:
+        click.echo("No Task found with number {}".format(nr))
+
+
