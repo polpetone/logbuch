fb84640af8254973de6e5e89f33344ed1b5569ea
Stefan Heiden <stefan.heiden@qudosoft.de>
2020-01-06 13:35:55 +0100

    view

--- src/portadapter/TaskView.py
+++ src/portadapter/TaskView.py
@@ -3,8 +3,9 @@ from datetime import datetime
 
 class TaskView(object):
 
-    def __init__(self, task):
+    def __init__(self, task, select_number):
         self.task = task
+        self.select_number = select_number
         self.date = datetime.fromisoformat(task.date).strftime("%d-%m-%Y %H:%M:%S")
         self.text = task.text
         self.status = task.status.status
--- src/portadapter/TasksView.py
+++ src/portadapter/TasksView.py
@@ -7,18 +7,22 @@ class TasksView(object):
         self.tasks = tasks
 
     def simple_table_view(self):
-        template = "{0:<30}{1:<40}{2:<20}\n"
+        template = "{0:<5}{1:<30}{2:<40}{3:<20}\n"
         gap_template = "{0:<100}\n"
-        sub_task_template = "{0:<20}{1:<30}{2:<40}{3:<20}\n"
-        out = template.format("Date", "Text", "Status")
+        sub_task_template = "{0:<20}{1:<5}{2:<30}{3:<40}{4:<20}\n"
+        out = template.format("Nr", "Date", "Text", "Status")
+        select_number = 0
         for task in self.tasks:
-            task_view = TaskView(task)
+            select_number += 1
+            task_view = TaskView(task=task, select_number=select_number)
             out += gap_template.format("----------------------------------------------------------------------------------")
-            out += template.format(task_view.date, task_view.text, task_view.status)
+            out += template.format(task_view.select_number, task_view.date, task_view.text, task_view.status)
             out += "\n"
+            sub_task_select_number = 0
             for sub_task in task.sub_tasks:
-                sub_task_view = TaskView(sub_task)
-                out += sub_task_template.format("", sub_task_view.date, sub_task_view.text, sub_task_view.status)
+                sub_task_select_number += 1
+                sub_task_view = TaskView(task=sub_task, select_number=sub_task_select_number)
+                out += sub_task_template.format("", sub_task_view.select_number, sub_task_view.date, sub_task_view.text, sub_task_view.status)
             out += "\n"
 
         return out
