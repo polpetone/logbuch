82da9f622cb1fed45dcf11e1ef536a0d291fc2ff
Stefan Heiden <stefan.heiden@qudosoft.de>
2020-01-06 11:54:36 +0100

    view

--- main.py
+++ main.py
@@ -17,12 +17,17 @@ def test1():
     task_service = TaskService()
     tasks = task_service.get_tasks()
     template = "{0:<30}{1:<40}{2:<20}\n"
-    out = template.format("Date", "Text", "Status")
+    sub_task_template = "{0:<20}{1:<30}{2:<40}{3:<20}\n"
+    out = template.format("Sub Task", "Date", "Text", "Status")
     for task in tasks:
+        out += "-----------------------------------------------------\n"
         out += template.format(task.date, task.text, task.status.status)
+        for sub_task in task.sub_tasks:
+            out += sub_task_template.format("", sub_task.date, sub_task.text, sub_task.status.status)
     return out
 
 
 if __name__ == "__main__":
+    test0()
     out = test1()
     print(out)
