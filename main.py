57918f9340a57fb0ea243a84b6c7d0d2d47ad445
Stefan Heiden <stefan.heiden@qudosoft.de>
2020-01-06 12:54:47 +0100

    view

--- main.py
+++ main.py
@@ -17,13 +17,16 @@ def test1():
     task_service = TaskService()
     tasks = task_service.get_tasks()
     template = "{0:<30}{1:<40}{2:<20}\n"
+    gap_template = "{0:<100}\n"
     sub_task_template = "{0:<20}{1:<30}{2:<40}{3:<20}\n"
     out = template.format("Sub Task", "Date", "Text", "Status")
     for task in tasks:
-        out += "-----------------------------------------------------\n"
+        out += gap_template.format("----------------------------------------------------------------------------------")
         out += template.format(task.date, task.text, task.status.status)
+        out += "\n"
         for sub_task in task.sub_tasks:
             out += sub_task_template.format("", sub_task.date, sub_task.text, sub_task.status.status)
+        out += "\n"
     return out
 
 
