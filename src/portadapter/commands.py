2998981ccf6b621aef9d50bc6539d4736bd2daf4
Stefan Heiden <stefan.heiden@qudosoft.de>
2020-01-29 07:24:34 +0100

    polishing

--- src/portadapter/commands.py
+++ src/portadapter/commands.py
@@ -61,26 +61,6 @@ def add_sub_task(nr, text):
 
 
 @logbuch.command()
-@click.option("--task-nr", type=click.IntRange(0, 20), prompt="task nr", help="Select a task by number")
-@click.option("--sub-task-nr", type=click.IntRange(0, 20), prompt="sub task nr", help="Select a task by number")
-@click.option("--status", prompt="status", help="OPEN, CANCELED, CLOSED")
-def change_status_deprecated(task_nr, sub_task_nr, status):
-    tasks = task_service.get_tasks()
-    tasks_view = TasksView(tasks)
-    task_view = tasks_view.get_task_view_by_number(task_nr)
-    if task_view:
-        sub_task_view = task_view.get_sub_task_view_by_number(sub_task_nr)
-        if sub_task_view:
-            sub_task_view.task.change_status(status)
-            task_service.save_tasks()
-        else:
-            task_view.task.change_status(status)
-            task_service.save_tasks()
-    else:
-        click.echo("No Task found with number {}".format(task_nr))
-
-
-@logbuch.command()
 @click.option("--uid", prompt="task id", help="Task id to change status for")
 @click.option("--status", prompt="status", help="OPEN, CANCELED, CLOSED")
 def change_status(uid, status):
