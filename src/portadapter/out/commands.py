f7df88c0074ae29d3680a2341e19cefc5104fdd0
stefan heiden <stefanheiden@gmail.com>
2020-03-26 20:31:32 +0100

    edit task text

--- src/portadapter/out/commands.py
+++ src/portadapter/out/commands.py
@@ -67,7 +67,9 @@ def edit_task(uid):
     if found_task:
         task_view = TaskView(found_task, None)
         altered_text = click.edit(task_view.detail_view())
+        task_view.parse_from_detail_view_string(altered_text)
         click.echo(altered_text)
+        click.echo(task_view.detail_view())
     else:
         click.echo("No Task found with uid {}".format(uid))
 
