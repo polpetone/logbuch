5eca407dbcf4d7eacc6b264c3259047739ceb9b7
Stefan Heiden <stefan.heiden@qudosoft.de>
2020-04-09 21:59:38 +0200

    Removed migration function

--- src/portadapter/input/migration/domain/LogFileService.py
+++
@@ -1,7 +0,0 @@
-class LogFileService:
-
-    def __init__(self, log_file_adapter):
-        self.log_file_adapter = log_file_adapter
-
-    def get_log_files(self, path: str):
-        return self.log_file_adapter.parse_log_files(path)
